7e1f5b41a977f49f516f6a0e7927b073158b53fd
Esraa Abduallah <esraahisham753@gmail.com>
2023-09-02 16:47:35 +0000

    complete redux config and add mock api

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from './components/App';
+import configStore from './redux/configStore';
+import {Provider as ReduxProvider} from 'react-redux';
 
+const store = configStore();
 
 render(
     <Router>
-        <App />
+        <ReduxProvider store={store} >
+            <App />
+        </ReduxProvider>
     </Router>, 
     document.getElementById("app"));
