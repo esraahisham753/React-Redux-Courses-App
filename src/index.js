0504f06fd02a3a11812e0ee33add2ced6aa3d8f9
Esraa Abduallah <esraahisham753@gmail.com>
2023-09-05 12:59:38 +0000

    Solve bootstrap injecting and create authors thunk and reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
+//import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
 import App from './components/App';
 import configStore from './redux/configStore';
